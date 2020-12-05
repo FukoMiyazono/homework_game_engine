0994bb682bf4e249bffa4eb6e427d1110367ecc7
FukoMiyazono <linyuxinxuan_14@126.com>
2020-12-05 12:25:49 +0800

    update homework09

---
+++ homework09/源代码/sound_test/Assets/blend.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class blend : MonoBehaviour
+{
+    public int volume;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void click()
+    {
+        AkSoundEngine.SetRTPCValue("RainVolume", volume);
+    }
+}
