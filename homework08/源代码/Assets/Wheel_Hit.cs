079ff8e2efabb51c098b44e1511245f0e14bc1c4
FukoMiyazono <linyuxinxuan_14@126.com>
2020-12-07 23:00:03 +0800

    update homework08

---
+++ homework08/源代码/Assets/Wheel_Hit.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wheel_Hit : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider col) {
+        if (col.isTrigger == true && col.gameObject.layer==11)
+        {
+            GameObject.Find("wheel_strike").GetComponent<ParticleSystem>().Play();
+        }
+     }
+
+    void OnTriggerExit(Collider col)
+    {
+        if (col.isTrigger == true)
+        {
+            GameObject.Find("wheel_strike").GetComponent<ParticleSystem>().Stop();
+        }
+    }
+}
